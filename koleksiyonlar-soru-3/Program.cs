01142be07edebc4033be711a690cb95e836ef3af
Batuhan Cömert <batuhancomert1@gmail.com>
2022-10-21 00:58:03 +0300

    Add files via upload

---
+++ koleksiyonlar-soru-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin elemanlarını sıralayan programı yazınız.
+
+            Console.WriteLine("Cümle Giriniz");
+            string sentence = Console.ReadLine();
+            string vowel = @"[aeıuüioö]";
+            Regex rg = new Regex(vowel);
+            MatchCollection vowelSounds = rg.Matches(sentence);
+
+            foreach (var item in vowelSounds)
+            {
+                Console.Write(item+",");
+            }
+            
+            
+        }
+    }
+}
